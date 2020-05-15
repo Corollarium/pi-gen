5e5813060fe35176fd5e2effdbe44e8251326610
Bruno <brunobg@gmail.com>
2020-05-15 10:06:51 -0300

    fix permissions, splash

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -5,18 +5,18 @@ VIDEOWALL_APPIMAGE=${VIDEOWALL_APPIMAGE:-/home/build/build/videowall/videowallcl
 rm -f "${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf"
 
 # install videowall
-install -m 777 ${VIDEOWALL_APPIMAGE}  ${ROOTFS_DIR}/home/pi/
-install -m 777 files/run-videowall.sh ${ROOTFS_DIR}/home/pi/
+install -m 777 -g pi -o pi ${VIDEOWALL_APPIMAGE}  ${ROOTFS_DIR}/home/pi/
+install -m 777 -g pi -o pi files/run-videowall.sh ${ROOTFS_DIR}/home/pi/
 
 # install autostart
-install -v -d                         ${ROOTFS_DIR}/home/pi/.config/lxsession/LXDE-pi
-install -v -m 644 files/autostart     ${ROOTFS_DIR}/home/pi/.config/lxsession/LXDE-pi/autostart
+install -v -d -g pi -o pi             ${ROOTFS_DIR}/home/pi/.config/lxsession/LXDE-pi
+install -v -m 644 files/autostart -g pi -o pi ${ROOTFS_DIR}/home/pi/.config/lxsession/LXDE-pi/autostart
 
 # Setting splash screen and background
 install -v -m 644 files/background.png ${ROOTFS_DIR}/usr/share/videowall/background.png
 install -v -d                         ${ROOTFS_DIR}/usr/share/plymouth/themes/pix/
 install -v -m 644 files/splash.png    ${ROOTFS_DIR}/usr/share/plymouth/themes/pix/splash.png
-install -v -d                         ${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/
-install -v -m 644 files/lxde-desktop  ${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+install -v -d -g pi -o pi             ${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/
+install -v -m 644 files/lxde-desktop -g pi -o pi ${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
 
 # cat files/.bashrc >> ${ROOTFS_DIR}/home/raspberrypi/.bashrc
