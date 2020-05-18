95b5336fe952e13bd3bbd4b3978de61591f32dcb
Bruno <brunobg@gmail.com>
2020-05-18 14:39:37 -0300

    fix install png

--- stage4/05-tweaks/00-run.sh
+++ stage4/05-tweaks/00-run.sh
@@ -13,10 +13,11 @@ install -v -d -o 1000 -g 1000             ${ROOTFS_DIR}/home/pi/.config/lxsessio
 install -v -m 644 files/autostart -o 1000 -g 1000 ${ROOTFS_DIR}/home/pi/.config/lxsession/LXDE-pi/autostart
 
 # Setting splash screen and background
-install -v -m 644 files/background.png ${ROOTFS_DIR}/usr/share/videowall/background.png
 install -v -d                         ${ROOTFS_DIR}/usr/share/plymouth/themes/pix/
 install -v -m 644 files/splash.png    ${ROOTFS_DIR}/usr/share/plymouth/themes/pix/splash.png
 install -v -d -o 1000 -g 1000             ${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/
 install -v -m 644 files/lxde-desktop -o 1000 -g 1000 ${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+install -v -d                         ${ROOTFS_DIR}/usr/share/videowall
+install -v -m 644 files/background.png ${ROOTFS_DIR}/usr/share/videowall/background.png
 
 # cat files/.bashrc >> ${ROOTFS_DIR}/home/pi/.bashrc
