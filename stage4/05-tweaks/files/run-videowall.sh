c017dca887c61e24037ead8b155aa04e3f8c0fce
Bruno <brunobg@gmail.com>
2020-07-22 18:05:42 -0300

    autodownloads in case of problems

--- stage4/05-tweaks/files/run-videowall.sh
+++ stage4/05-tweaks/files/run-videowall.sh
@@ -5,10 +5,14 @@ shopt -s nullglob
 IMAGES=(videowallclient-linux-*)
 shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
 
-if (( ${#IMAGES[@]} == 0 )); 
+if (( ${#IMAGES[@]} != 1 )); 
 then
-        # TODO wget
+        rm -f videowallclient-linux-*
+        wget --content-disposition 'https://corollarium.com/product/4329/download?filename=videowallclient-linux-latest-armv7l.AppImage&automaticDownload=true'
         echo "Downloading..."
+        shopt -s nullglob
+        IMAGES=(videowallclient-linux-*)
+        shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
 fi
 
 $(/home/pi/${IMAGES[0]})
