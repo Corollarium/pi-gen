f458b3c5de56d78c15b228035452f0573677843c
Bruno <brunobg@gmail.com>
2020-08-05 18:02:16 -0300

    handling corruption

--- stage4/05-tweaks/files/run-videowall.sh
+++ stage4/05-tweaks/files/run-videowall.sh
@@ -3,23 +3,32 @@
 # exec 19>/tmp/logfile
 # BASH_XTRACEFD=19
 
-cd /home/pi
-shopt -s nullglob
-IMAGES=(videowallclient-linux-*)
-shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
-
-if (( ${#IMAGES[@]} != 1 )); 
-then
-        rm -f videowallclient-linux-*
-        while true;
-        do
-                wget --verbose -T 15 --content-disposition 'https://corollarium.com/product/4329/download?filename=videowallclient-linux-latest-armv7l.AppImage&automaticDownload=true' && break
-        done
-        echo "Downloading..."
+while true; do
+        cd /home/pi
         shopt -s nullglob
         IMAGES=(videowallclient-linux-*)
         shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
-        $(chmod +x /home/pi/${IMAGES[0]})
-fi
 
-$(/home/pi/${IMAGES[0]})
+        if (( ${#IMAGES[@]} != 1 )); 
+        then
+                rm -f videowallclient-linux-*
+                while true;
+                do
+                        wget --verbose -T 15 --content-disposition 'https://corollarium.com/product/4329/download?filename=videowallclient-linux-latest-armv7l.AppImage&automaticDownload=true' && break
+                done
+                echo "Downloading..."
+                shopt -s nullglob
+                IMAGES=(videowallclient-linux-*)
+                shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
+                $(chmod +x /home/pi/${IMAGES[0]})
+        fi
+
+        $(/home/pi/${IMAGES[0]})
+
+        if (( $? == 127 ));
+        then 
+                rm -f videowallclient-linux-*
+        fi
+
+        sleep 5
+done
